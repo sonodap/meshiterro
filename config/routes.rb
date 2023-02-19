29538725f9ee26dc4a0dff55ea6bfae62c73e8b2
EC2 Default User <ec2-user@ip-172-31-27-249.ap-northeast-1.compute.internal>
2023-02-19 15:48:13 +0000

    app2-19-1

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to:  "homes#top"
   get 'homes/about' => 'homes#about',as: "about"
   resources :post_images, onry:[:new,:create,:index,:show,:destroy] do
+    resource :favorites, onry:[:create,:destroy]
     resources :post_comments, onry: [:create,:destroy]
   end  
   resources :users, onry:[:show,:edit,:update]
